3ca6f55f38eb63df9daef977c37e6981b97f7c2e
Sambit Mishra <labsquantumleap@gmail.com>
2026-09-18 23:21:56 +0530

    PR28: export quorum replication primitives

--- exchange_core/src/lib.rs
+++ exchange_core/src/lib.rs
@@ -15,6 +15,7 @@ pub mod order;
 pub mod pool;
 pub mod replay;
 pub mod replication;
+pub mod quorum;
 pub mod ring;
 pub mod scavenger;
 pub mod sequence;
@@ -50,6 +51,7 @@ pub use replay::{
     replay_bytes, replay_file, ReplayError, ReplayLedger, ReplayOrderKey, ReplayTrade,
 };
 pub use replication::{Generation, HotStandby, ReplicationError, Sequencer};
+pub use quorum::{CommitIndex, QuorumError, ReplicatedEntry, ReplicationLog, QuorumTracker};
 pub use ring::{OrderQueue, RingBuffer};
 pub use sequence::SequenceNumber;
 pub use snapshot::{ReplaySnapshot, SnapshotError, SnapshotMeta};
